924a5e03dd5c38f919b43f1d1fe15ad10c6a9229
jhow <jho_santos_jho@hotmail.com>
2022-04-05 18:04:08 -0300

    ♻️  added components on index file

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,10 @@
 export { FieldInput } from "./FieldInput";
 export { FormLogin } from "./FormLogin";
-export { Button } from "./Button"
+export { Button } from "./Button";
+export { WarningText } from './WarningText';
+export { LinkText } from './LinkText'
+export { Accordion } from './Accordion'
+export { InputText } from './InputText'
+export { Modal } from './Modal'
+export { ToggleSwitch } from './ToggleSwitch'
 
